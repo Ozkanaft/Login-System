a41f5268924197f97c0c275d0c409a2be70b3ee9
Daniel Sauer <daniel.s93@gso.schule.koeln>
2023-03-31 10:16:56 +0200

    Default Projekt
    hier wurde das Grundgerüst von HTML eingefügt,
    womit das Projekt einen Ansatzpunkt hat.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="de"> <!-- Sprache der Website -->
+  <head>
+    <meta charset="utf-8"> <!-- Dekodierungstyp -->
+    <title>Login-System</title>
+    <style>
+    </style>
+  </head>
+  <body>
+  </body>
+</html>
