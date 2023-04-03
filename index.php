57836726709dbb570dad66a528fda401669bfb6c
Daniel Sauer <daniel.s93@gso.schule.koeln>
2023-04-03 16:06:57 +0200

    Separierung der Funktionen
    Folgende Änderungen wurden vollbracht:
    
    - Die eingegebenen Daten werden auf einer seperaten Website unter dem
      Namen "Persönliche Daten" ausgegeben. Dafür wurde eine neue php-Datei
      mit dem Namen "datenausgabe.php" angelegt.
    
    - Statt nur dem Vor- und Nachnamen kann der nutzer auch einen
      Benutzernamen und ein Passwort eingeben.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8">
 
         <!-- Tab-Titel der Website -->
-        <title>Login-System</title>
+        <title>Registrierung</title>
 
         <!-- Designerische Aspekte -->
         <style>
@@ -25,16 +25,16 @@
         </style>
     </head>
     <body>
-        <h1>Login-System</h1>
-        <p>Hallo Nutzer! Bitte regestrieren Sie sich:</p>
+        <h1>Registrierung:</h1>
+        <p>Hallo Nutzer! Bitte registrieren Sie sich:</p>
         
-        <!-- Das Einagbe-Formular -->
+        <!-- Das Einagbe-Formular fürs Registrieren -->
         <div class="container">
 
             <!-- Formularerzeugung -->
-            <form action="index.php" method="post" target="ausgabe">
+            <form action="datenausgabe.php" method="post" target="ausgabe">
                 <fieldset>
-                    <legend>Persönliche Daten:</legend>
+                    <!-- Steuerlemente für die Dateneingabe -->
                     <p>
                         <label for="vorname">Vorname: </label>
                         <input type="text" name="vorname" id="vorname" />
@@ -44,29 +44,22 @@
                         <input type="text" name="nachname" id="nachname" />
                     </p>
                     <p>
+                        <label for="benutzername">Benutzername: </label>
+                        <input type="text" name="benutzername" id="benutzername" />
+                    </p>
+                    <p>
+                        <label for="passwort">Passwort: </label>
+                        <input type="password" name="passwort" id="passwort" />
+                    </p>
+                    <p>
+                        <!-- Button für die Formular-Absendung -->
                         <input type="submit" name="submit" />
+
+                        <!-- Button fürs Zurücksetzen -->
                         <input type="reset" />
                     </p>
                 </fieldset>
             </form>
         </div>
-
-        <!-- Die Ausgabe -->
-        <div id="ausgabe">
-            <?php
-                // Falls ein Formular unter dem Namen "submit" abgeschickt worden ist
-                if (isset($_POST["submit"]))
-                {
-                    // Initialisierung der Variablen durch die eingegebene Werte
-                    $vorname = $_POST["vorname"];
-                    $nachname = $_POST["nachname"];
-                    
-                    // Ausschreibung der Variablen (mithilfe von Punktbindungen)
-                    echo "<p>";            
-                    echo "Hallo, ".$vorname." ".$nachname.".";
-                    echo "</p>";
-                }   
-            ?>
-        </div>    
     </body>
 </html>
