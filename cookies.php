75787025891b4178581c0a46341d9926e2188b90
Daniel Sauer <daniel.s93@gso.schule.koeln>
2023-04-14 11:08:24 +0200

    Optimierung der Cookie-Einstellung
    Falls der Nutzer Cookies akzeptiert hat, dann werden seine Daten von
    dem Cookie angezeigt und er kann sie nicht mehr ablehnen.
    
    Falls der Nutzer Cookies abgelehnt hat, dann werden seine Daten von
    der Session angezeigt und er kann sie nicht mehr annehmen.

--- cookies.php
+++ cookies.php
@@ -1,28 +1,29 @@
 <!-- Skript für die Cookies -->
 <?php
+
     // Keine automatisch generierte Fehlermeldungen auf der loaklen Website 
     ini_set("display_errors", 0);
 
+    // Fortführung der laufenden Sitzung
+    session_start();
+    
+    // Der Cookie besitzt die ID des Datensatzes als Name
+    $cookie_name = $_SESSION["session_id"];
+    
+    // Zuweisung der Session-Daten in einem Array (um ihn als den "Cookie-Wert" benutzen zu können)
+    $cookie_wert = array(
+        "cookie_vorname" => $_SESSION["session_vorname"],
+        "cookie_nachname" => $_SESSION["session_nachname"],
+        "cookie_email" => $_SESSION["session_email"],
+        "cookie_benutzername" => $_SESSION["session_benutzername"],
+        "cookie_passwort" => $_SESSION["session_passwort"]
+    );
+    
+    // Erzeugung eines Cookies mit einer Lebensdauer von 1 Tag
+    // (Die Cookie-Merkmale werden in ein JSON String umgewandelt, damit sie als Merkmale anerkannt werden können)
+    setcookie($cookie_name, json_encode($cookie_wert), time() + 86400, "/");
+
     if (isset($_POST["cookies_ja"])) {
-
-        // Fortführung der laufenden Sitzung
-        session_start();
-
-        // Der Cookie besitzt die ID des Datensatzes als Name
-        $cookie_name = $_SESSION["session_id"];
-
-        // Zuweisung der Session-Daten in einem Array (um ihn als den "Cookie-Wert" benutzen zu können)
-        $cookie_wert = array(
-            "cookie_vorname" => $_SESSION["session_vorname"],
-            "cookie_nachname" => $_SESSION["session_nachname"],
-            "cookie_email" => $_SESSION["session_email"],
-            "cookie_benutzername" => $_SESSION["session_benutzername"],
-            "cookie_passwort" => $_SESSION["session_passwort"]
-        );
-
-        // Erzeugung eines Cookies mit einer Lebensdauer von 5 Minuten
-        // (Die Cookie-Merkmale werden in ein JSON String umgewandelt, damit sie als Merkmale anerkannt werden können)
-        setcookie($cookie_name, json_encode($cookie_wert), time() + 300, "/");
 
         // Dekodierung der Cookie-Daten von einem JSON Strings in ein array:
         $cookie_wert = json_decode($_COOKIE[$cookie_name], true);
@@ -37,14 +38,26 @@
         echo "<li> Nachname: " . $cookie_wert["cookie_nachname"] . "</li>";
         echo "<li> E-Mail: " . $cookie_wert["cookie_email"] . "</li>";
         echo "</ul>";
+    
+        // Die HTML-Klasse "cookies_anzeige" wird ausgeblendet
+        echo "<style>.cookies_anzeige { display: none }</style>";
     }
 
     if (isset($_POST["cookies_nein"])) {
 
-        echo "<p> Sie haben Cookies abgelehnt. </p>";
+        echo "<h3> Sie haben Cookies abgelehnt! </h3>";
 
-        // Erzeugung eines Cookies mit einer Lebensdauer von 0 Sekunden und keinem Wert
-        setcookie("nutzer", "", time() - 1, "/");
+        // Zeige den Datensatz der Sitzung an
+        echo "<p> Hallo, " . $_SESSION["session_benutzername"] . "!</p>";
+        echo "Dies sind Ihre persönliche Daten:<br>";
+        echo "<ul>";
+        echo "<li> Vorname: " . $_SESSION["session_vorname"] . "</li>";
+        echo "<li> Nachname: " . $_SESSION["session_nachname"] . "</li>";
+        echo "<li> E-Mail: " . $_SESSION["session_email"] . "</li>";
+        echo "</ul>";
+
+        // Die HTML-Klasse "cookies_anzeige" wird ausgeblendet
+        echo "<style>.cookies_anzeige { display: none }</style>";
     }
 
     if (isset($_POST["cookies_login"])) {
