8fcc72fd360e2755495a20ae0bacd27a4a47d299
Daniel Sauer <daniel.s93@gso.schule.koeln>
2023-04-17 14:42:46 +0200

    Behebung der nutzer.php Ausgangs-Funktion
    Falls bei der Nutzerdaten-Ausgabe (durch einen Cookie) auf den Button
    "Zurück zum Menü" oder "Zurück zum Login" geklickt wird, wird die
    nutzer.php Datei verlassen.

--- nutzer.php
+++ nutzer.php
@@ -1,19 +1,23 @@
 <!-- Skript für die Nutzerdaten-Ausgabe durch einen Cookie -->
 <?php
-    // Keine automatisch generierte Fehlermeldungen auf der loaklen Website 
+    // Keine automatisch generierte Fehlermeldungen auf der loaklen Website
     ini_set("display_errors", 0);
 
-    // Fortführung der Sitzung
-    session_start();
+    // Falls auf einen Link mit der URL "link_geklickt" geklickt wurde
+    if (isset($_GET["link_geklickt"])) {
 
-    // Zeige die Sitzungs-Daten an
-    echo "<p> Hallo, " . $_SESSION["session_benutzername"] . "!</p>";
-    echo "Dies sind Ihre persönliche Daten:<br>";
-    echo "<ul>";
-    echo "<li> Vorname: " . $_SESSION["session_vorname"] . "</li>";
-    echo "<li> Nachname: " . $_SESSION["session_nachname"] . "</li>";
-    echo "<li> E-Mail: " . $_SESSION["session_email"] . "</li>";
-    echo "</ul>";
+        // Fortführung der Sitzung
+        session_start();
+
+        // Zeige die Sitzungs-Daten an
+        echo "<p> Hallo, " . $_SESSION["session_benutzername"] . "!</p>";
+        echo "Dies sind Ihre persönliche Daten:<br>";
+        echo "<ul>";
+        echo "<li> Vorname: " . $_SESSION["session_vorname"] . "</li>";
+        echo "<li> Nachname: " . $_SESSION["session_nachname"] . "</li>";
+        echo "<li> E-Mail: " . $_SESSION["session_email"] . "</li>";
+        echo "</ul>";
+    }
 
     if (isset($_POST["nutzer_login"])) {
 
